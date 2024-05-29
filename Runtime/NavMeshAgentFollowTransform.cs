f780e06884ce612052e86d7f4191e889d5cce4c8
ludachrismonte <ludachrismonte@gmail.com>
2024-05-29 09:32:04 -0500

    rotating

--- Runtime/NavMeshAgentFollowTransform.cs
+++ Runtime/NavMeshAgentFollowTransform.cs
@@ -16,7 +16,11 @@ namespace IronMountain.NavigationUtilities
         [SerializeField] public Transform target;
         [SerializeField] public RotationType rotationType = RotationType.LookAtTarget;
         [SerializeField] [Range(0, 12)] public float rotationSpeed = 6;
+        [SerializeField] [Range(0, 180)] public float idealAngleRange = 6;
+        [SerializeField] [Range(0, 180)] public float maximumAngleRange = 6;
 
+        private bool _isRotating;
+        
         public bool HasRemainingDistance => 
             navMeshAgent
             && navMeshAgent.hasPath
@@ -24,6 +28,8 @@ namespace IronMountain.NavigationUtilities
 
         private void OnValidate()
         {
+            maximumAngleRange = Mathf.Clamp(maximumAngleRange, 0, 180);
+            idealAngleRange = Mathf.Clamp(idealAngleRange, 0, maximumAngleRange);
             if (!navMeshAgent) navMeshAgent = GetComponent<NavMeshAgent>();
         }
 
@@ -43,28 +49,28 @@ namespace IronMountain.NavigationUtilities
             {
                 navMeshAgent.ResetPath();
 
-                Vector3 lookDirection = Vector3.zero;
-
-                switch (rotationType)
+                Vector3 currentDirection = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+                Vector3 idealDirection = rotationType switch
                 {
-                    case RotationType.AlignWithTarget:
-                        lookDirection = target.forward;
-                        break;
-                    case RotationType.LookAtTarget:
-                        lookDirection = target.position - transform.position;
-                        break;
-                }
-
-                lookDirection = Vector3.ProjectOnPlane(lookDirection, Vector3.up).normalized;
-
-                if (lookDirection != Vector3.zero)
+                    RotationType.AlignWithTarget => Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized,
+                    RotationType.LookAtTarget => Vector3.ProjectOnPlane(target.position - transform.position, Vector3.up).normalized,
+                    _ => Vector3.zero
+                };
+                
+                if (idealDirection == Vector3.zero || currentDirection == Vector3.zero) return;
+                
+                float angle = Vector3.Angle(idealDirection, currentDirection);
+                _isRotating = angle > maximumAngleRange || (_isRotating && angle > idealAngleRange);
+                if (_isRotating)
                 {
-                    Quaternion lookRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-                    transform.rotation =
-                        Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+                    Quaternion idealRotation = Quaternion.LookRotation(idealDirection, Vector3.up);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, idealRotation, Time.deltaTime * rotationSpeed);
                 }
             }
-            else navMeshAgent.SetDestination(target.position);
+            else if (navMeshAgent.destination != target.position)
+            {
+                navMeshAgent.SetDestination(target.position);
+            }
         }
     }
 }
